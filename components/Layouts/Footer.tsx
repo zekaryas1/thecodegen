f96e7eaa73a8dc2f380cd42c5148f9b3454bf1b5
zekaryas <zekaryasdinku@gmail.com>
2023-04-26 08:32:48 +0300

    updated footer content

--- components/Layouts/Footer.tsx
+++ components/Layouts/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Footer() {
   return (
     <p className="text-center text-gray-200 bg-gray-900 p-3">
-      Codegen is Made by Zekaryas Tadele @{" "}
+      Made by Zekaryas Tadele @{" "}
       <Link href="https://github.com/zekaryas1/thecodegen">Github</Link>
     </p>
   );
