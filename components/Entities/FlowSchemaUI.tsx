2fbfd70db22566836180681d49d5089ebaac5376
zekaryas <zekaryasdinku@gmail.com>
2023-05-18 11:11:20 +0300

    removed table ui option

--- components/Entities/FlowSchemaUI.tsx
+++ components/Entities/FlowSchemaUI.tsx
@@ -1,39 +1,65 @@
-import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 import { Handle, Position } from "reactflow";
 import { Entity } from "../../lib/models/Entity";
+import { Column } from "../../lib/models/Column";
 
 interface FlowSchemaUIProps {
   data: Entity;
 }
 
 function FlowSchemaUI({ data }: FlowSchemaUIProps) {
-  const headerBody = () => {
-    return <p className="text-primary"> <i className="pi pi-table" /> {data.name}</p>;
-  };
-
-  const typeBody = (data: any) => {
-    return <p className="font-italic text-xs">{data.type}</p>;
-  };
-
   return (
-    <div className="bg-red-100 w-12rem">
-      <Handle type="target" position={Position.Top} />
-      <DataTable
-        value={data.columns}
-        header={headerBody}
-        stripedRows
-        size="small"
-        className="p-datatable-gridlines p-datatable-sm"
-      >
-        <Column field="name" header="name"></Column>
-        <Column field="type" header="type" body={typeBody}></Column>
-      </DataTable>
-      <Handle type="source" position={Position.Bottom} id="a" />
-      <Handle type="source" position={Position.Bottom} id="b" />
+    <div className="bg-gray-700 p-2 w-12rem">
+      <h3 className="mb-2">{data.name}</h3>
+      {data?.columns?.map(function (column: Column, i: number) {
+        return (
+          <div key={column.id} className="flex bg-gray-500 mb-2 p-2">
+            {data.name && (
+              <ROW
+                key={column.id}
+                column={column}
+                index={i}
+                entityName={data.name}
+              />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 export default FlowSchemaUI;
+
+function ROW({
+  column,
+  index,
+  entityName,
+}: {
+  column: Column;
+  index: number;
+  entityName: string;
+}) {
+  const ref = useRef(null);
+
+  return (
+    <>
+      <Handle
+        type="source"
+        position={Position.Right}
+        id={`${entityName}.${column.name}.s`}
+        style={{ top: 42.5 * index + 60 }}
+      />
+      <p>
+        {column.name} - {column.type}
+      </p>
+      <Handle
+        type="target"
+        position={Position.Left}
+        id={`${entityName}.${column.name}.t`}
+        style={{ top: 42.5 * index + 60 }}
+      />
+    </>
+  );
+}
